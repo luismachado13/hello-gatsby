665f6d410a1d8044c5bdd9dc1d0b380cab80d119
Luis  Machado <luismachado@MacBook-Pro-de-Luis.local>
2020-05-12 22:32:54 -0300

    Meu teste

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,8 @@ export default function Home({data}) {
       <p>Site exemplo em Gatsby do</p>
       <p>TEstes</p>
       <p>TEstes</p>
+      <p>TEstes</p>
+      <p>TEstes</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
       <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
