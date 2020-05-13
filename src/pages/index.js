ac8196ed29e045339f6abc5381ddde3684c1d4b6
Luis  Machado <luismachado@MacBook-Pro-de-Luis.local>
2020-05-12 22:31:41 -0300

    Outro Teste

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ export default function Home({data}) {
       <Header headerText="Hello Gatsby!" />
       <p>Site exemplo em Gatsby do</p>
       <p>TEstes</p>
+      <p>TEstes</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
       <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
